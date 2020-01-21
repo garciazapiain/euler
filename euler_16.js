73f80120b010f69d94839ef063b2065be4b28e6d
garciazapiain <juangarciazapiain@gmail.com>
2020-01-21 08:54:38 +0100

    euler 16

---
+++ euler_16.js
@@ -0,0 +1,17 @@
+// 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+// What is the sum of the digits of the number 2^1000?
+
+let n = BigInt(2**1000);
+let digits = (""+n).split("");
+let sum = 0;
+
+// console.log(n);
+// console.log(digits);
+
+for (i=0;i<digits.length;i++){
+    sum = parseFloat(digits[i]) + sum;
+    console.log(sum)
+}
+
+
